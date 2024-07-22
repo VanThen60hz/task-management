2315585fdbd2c3b38b709f1d0076724f874d0cd3
VanThen60hz <nguyenvthang2409@gmail.com>
2024-07-22 20:51:52 +0700

    config

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        document.querySelector(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                document.querySelector(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        });
+}
